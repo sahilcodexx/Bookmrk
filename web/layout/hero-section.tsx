edb91f2095ec51b096fd3c84d93cc499dcad6c9e
sahilcodexx <sahil171759@gmail.com>
2026-04-17 00:55:17 +0530

    Fix dark mode background color

--- web/layout/hero-section.tsx
+++ web/layout/hero-section.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/container";
 
 const Hero = () => {
   return (
-    <Container className="min-h-screen px-10 py-5">
+    <Container className="min-h-screen px-10 py-5 ">
       <h2 className="text-3xl">Hero</h2>
     </Container>
   );
