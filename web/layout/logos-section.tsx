303232d529f6afe0f1b7911a641abe384596bd02
sahilcodexx <sahil171759@gmail.com>
2026-04-19 02:30:00 +0000

    Add animation and styling to logos section

--- web/layout/logos-section.tsx
+++ web/layout/logos-section.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Container from "@/components/container";
 import { motion } from "motion/react";
@@ -17,15 +17,18 @@ const LogoSection = () => {
 
   return (
     <Container>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="min-h-screen flex flex-col pt-20 gap-10 items-center justify-start">
+        <h2 className="text-4xl max-w-sm text-center">
+          People Use Our Product Working At
+        </h2>
+        <div className="grid  grid-cols-4  ">
           {companies.map((company, index) => (
             <motion.div
               key={company.name}
-              initial={{ opacity: 0, filter: "blur(10px)" }}
-              animate={{ opacity: 1, filter: "blur(0px)" }}
+              initial={{ opacity: 0, filter: "blur(10px)", y: 10 }}
+              animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className="flex items-center justify-center text-2xl font-bold"
+              className="flex items-center justify-center px-25 py-8 border "
             >
               {company.name}
             </motion.div>
