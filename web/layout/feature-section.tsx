c0afdd8df30321e89b9f7d4f4cd8cf3a871c847c
sahilcodexx <sahil171759@gmail.com>
2026-04-21 02:30:00 +0000

    feature section initlize

--- web/layout/feature-section.tsx
+++ web/layout/feature-section.tsx
@@ -3,11 +3,22 @@ import React from "react";
 
 const FeatureSection = () => {
   return (
-    <Container>
-      <div className="min-h-screen flex items-center justify-center">
-        FeatureSection
-      </div>
-    </Container>
+    <>
+      <Container>
+        <div className="min-h-screen  py-10">
+          <div className="border-y flex items-center h-100 justify-center gap-10 px-10 ">
+            <div className="flex w-full items-center justify-center ">
+              <div className="h-60 border w-60 bg-secondary/30 rounded-lg  flex items-center justify-center">
+                s
+              </div>
+            </div>
+            <div className=" bg-neutral-300/70 dark:bg-neutral-700/50 w-px h-full "></div>
+            <div className="w-full">s</div>
+          </div>
+        </div>
+      </Container>
+      <div className=" bg-neutral-300/70 dark:bg-neutral-700/50 h-px w-full "></div>
+    </>
   );
 };
 
